3a6662f691bc0e64f1ee568eb473728ccc2fa765
UTkarsh Maheshwari <utkarshme96@gmail.com>
2018-04-24 21:40:43 +0530

    Indicate packets being sent (temporarily)

--- src/sender.c
+++ src/sender.c
@@ -2,16 +2,19 @@
 #include <sys/socket.h>
 
 #include "core.h"
-#include "udt.h"
 #include "sender.h"
+#include "buffer.h"
 
 void sender_start (void *arg)
 {
     /* TODO: CAN ONLY SEND TO AN ADDRESS IN UDP */
 
     /*conn_t *connection = (conn_t *) arg;*/
-    /*char buffer[] = "This is a message";*/
-    /*while (send(connection -> sock, buffer, sizeof(buffer), 0)) {*/
-        /*printf("Sent a packet\n");*/
-    /*}*/
+    char buffer[] = "This is a message";
+
+    while (1) {
+        if (send_buffer_read(buffer, sizeof(buffer))) {
+            printf("Send: %s\n", buffer);
+        }
+    }
 }
