f41f77cb85e481939ca6991bb6397c9d7cdae505
Vivek Kumar <arya.vkm001@gmail.com>
2018-04-27 11:21:13 +0530

    updated send_buffer.c
    added definition for send_file_buffer_write() as called by udt_sendfile().

--- src/send_buffer.c
+++ src/send_buffer.c
@@ -1,4 +1,7 @@
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "buffer.h"
 #include "packet.h"
@@ -70,13 +73,61 @@ int send_buffer_write(char *data, int len)
 
 int send_file_buffer_write(int fd, int offset, int size, int blocksize)
 {
-    int pos = offset;
+			
     packet_t packet;
+
+	off_t pos;	
     int retval;
+    int seqnum;
+	char *buffer;	
+    int boundary;
+	int readbytescount;
+	int packet_dsize;
 
-    while (1) {
-        /* TODO: read file and create packets. */
+	pos			= offset;
+    retval 		= size;
+    seqnum 		= 2142894844;  /* TODO: generate random number */
+    buffer 		= (char *) malloc (size);
+    boundary 	= PACKET_BOUNDARY_START;	
+	
+	if (fd < 0 || pos < 0) return -1;
+
+	packet_dsize = (blocksize > PACKET_DATA_SIZE) ? PACKET_DATA_SIZE : blocksize;
+		
+	while (size > 0)
+    {
+		
+		lseek(fd, pos, SEEK_SET);
+		readbytescount 	 = read(fd, buffer, packet_dsize);
+		size			-= readbytescount;
+
+        boundary = boundary | (size > 0) ? PACKET_BOUNDARY_NONE : PACKET_BOUNDARY_END;
+
+        packet_clear_header (packet);
+        packet_set_data     (packet);
+        packet_set_seqnum   (packet, seqnum++);
+        packet_set_boundary (packet, boundary);
+        packet_set_order    (packet, 1);
+        packet_set_msgnum   (packet, 1);
+        packet_set_timestamp(packet, 0x0000051c); /* TODO: calculate time */
+        packet_set_id       (packet, 0x08c42c74); /* TODO: generate an id */
+
+        packet_new(&packet, buffer, readbytescount);
+        send_packet_buffer_write(&packet);
+
+        boundary = PACKET_BOUNDARY_NONE;
+	
+		pos += blocksize;
     }
+		
+	packet_clear_header (packet);
+    packet_set_ctrl     (packet);
+    packet_set_type     (packet, PACKET_TYPE_ACK);
+    packet_set_timestamp(packet, 0x0000051c); /* TODO: calculate time */
+    packet_set_id       (packet, 0x08c42c74); /* TODO: generate an id */
 
-    return retval; /* TODO: calculate bytes sent and return */
+    packet_new(&packet, NULL, 0);
+    send_packet_buffer_write(&packet);
+				
+    return retval; /* return total bytes sent */		
 }
