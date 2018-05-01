c229178c037dc4f4f102f37b07c6a12d522b3964
UTkarsh Maheshwari <utkarshme96@gmail.com>
2018-05-01 23:05:41 +0530

    Check size while writing to file (Fix #8)

--- src/recv_buffer.c
+++ src/recv_buffer.c
@@ -21,21 +21,24 @@ int recv_buffer_read(char *data, int len)
     return buffer_read(&buffer, data, len);
 }
 
-int64_t recv_file_buffer_read(int fd, int64_t *offset, int64_t size, int64_t blocksize)
+int64_t recv_file_buffer_read(int fd, int64_t *offset, int64_t size,
+                              int64_t blocksize)
 {
     char data[PACKET_DATA_SIZE];
     int retval = 0;
     int len = 0;
     int read;
+    int bytes_to_write;
 
     if (fd < 0) return -1;
 
     while (size > 0) {
 
         read = buffer_read(&buffer, data, PACKET_DATA_SIZE);
-        if (read == 0) continue;
+        if (read == 0) continue;  /* keep polling until there is data */
 
-        len = pwrite(fd, &data, PACKET_DATA_SIZE, *offset);
+        bytes_to_write = (size > PACKET_DATA_SIZE) ? PACKET_DATA_SIZE : size;
+        len = pwrite(fd, &data, bytes_to_write, *offset);
         if (len < 1) return len;
 
         *offset += len;
