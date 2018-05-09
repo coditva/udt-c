486764f7ae43c50ae223d43eaa73a69360e895a1
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-05-09 15:08:12 +0530

    Initialize attributes before creating thread

--- src/util.c
+++ src/util.c
@@ -7,6 +7,9 @@ tid_t thread_start (thread_worker_t worker, void *args)
     tid_t tid = -1;
     pthread_attr_t attr;
 
+    /* init thread attributes */
+    pthread_attr_init(&attr);
+
     pthread_create(&tid, &attr, worker, args);
     return tid;
 }
